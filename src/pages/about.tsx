40e5613a4601c6721d4a6e4f1f0854d62383d030
blake <blake@infrablockchain.com>
2022-04-25 17:00:35 +0900

    add sample pages

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,43 @@
+import { CSSObject } from "@emotion/react";
 import Layout from "../components/Layout";
+import { mq } from "../styles/breakpoints";
+
+const aboutStyles: CSSObject = {
+  width: "100%",
+  height: "auto",
+  padding: "200px",
+  "& > p": {
+    width: "50%",
+  },
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "flex-start",
+  alignItems: "flex-start",
+  lineHeight: "170%",
+  [mq[0]]: {
+    padding: "50px 0",
+    "& > p": {
+      width: "100%",
+    },
+  },
+};
 
 const About = () => {
   return (
     <Layout>
-      <section>
+      <section css={aboutStyles}>
         <h1>hello!</h1>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </p>
       </section>
     </Layout>
   );
